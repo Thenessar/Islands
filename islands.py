a74f7fa2782ea34ed967a9baa1669f5eaf47d31c
Thenessar <108839015+Thenessar@users.noreply.github.com>
2022-11-30 00:11:24 +0100

    Added Islands class

---
+++ islands.py
@@ -0,0 +1,18 @@
+class Islands:
+    def __init__(self, map):
+        self.map = map
+
+    def clean_the_map():
+        pass
+
+    def check_map_appearance():
+        pass
+
+    def find_number_of_islands():
+        pass
+
+    def check_move_possibility():
+        pass
+
+    def find_neighbours():
+        pass
